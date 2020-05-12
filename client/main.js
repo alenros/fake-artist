dcc47f0d9e01b07dc9e4a01964f2a5a7b1a3a76f
simon s <gel@gmx.com>
2020-05-12 18:49:48 +0300

    Fixed game starting with empty user word or category

--- client/main.js
+++ client/main.js
@@ -436,6 +436,10 @@ Template.lobby.events({
     let game = getCurrentGame();
     let word = document.getElementById("user-word").value;
     let category = document.getElementById("user-category").value;
+    if(word.length == 0 || category.length == 0)
+    {
+      return;
+    }
 
     (function(name,path,ctx){ctx[name]=ctx[name]||{ready:function(fn){let h=document.getElementsByTagName('head')[0],s=document.createElement('script'),w=window,loaded;s.onload=s.onerror=s.onreadystatechange=function(){if((s.readyState&&!(/^c|loade/.test(s.readyState)))||loaded){return}s.onload=s.onreadystatechange=null;loaded=1;ctx[name].ready(fn)};s.async=1;s.src=path;h.parentNode.insertBefore(s,h)}}})
     ('KeenTracking', 'https://cdn.jsdelivr.net/npm/keen-tracking@4/dist/keen-tracking.min.js', this);
